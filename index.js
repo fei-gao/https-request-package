77f2ff82cfa899754f0fd8524c4f180596ae413c
Fei Gao <fgao1@ualberta.ca>
2018-09-03 12:58:01 -0600

    add comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let request = require('request');
 let fs = require('fs');
 
+//request is a stream / asynchronous, 
+//and streams generally respond to a certain four functions 
 request.get('https://sytantris.github.io/http-examples/future.jpg')
 .on('error', function(err){
     throw err;
@@ -10,6 +12,11 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
     console.log('Response Status Code: ', response.statusCode, "\nresponse message is", response.statusMessage, "\ncontent type is", response.headers['content-type']);
 })
 
+//Pipes are an interface for converting 
+//between readable and writable streams.
+
+//fs.createWriteStream handles the incoming readable data stream, 
+//transforming it into a writable data stream
 .pipe(fs.createWriteStream('./future.jpg'))
 
 .on('finish', function(){
