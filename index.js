3974d82f9b59ff627a6a2903046da7595511e859
Fei Gao <fgao1@ualberta.ca>
2018-09-04 23:00:25 -0600

    edit request stream

--- index.js
+++ index.js
@@ -12,15 +12,14 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
     console.log('Response Status Code: ', response.statusCode, "\nresponse message is", response.statusMessage, "\ncontent type is", response.headers['content-type']);
 })
 
+.on('end', function(){
+    console.log('Download complete.')
+})
+
 //Pipes are an interface for converting 
 //between readable and writable streams.
 
 //fs.createWriteStream handles the incoming readable data stream, 
 //transforming it into a writable data stream
-.pipe(fs.createWriteStream('./future.jpg'))
+.pipe(fs.createWriteStream('./future.jpg'));
 
-.on('finish', function(){
-    console.log('Download complete.')
-});
-
-console.log('Downloading image...');
